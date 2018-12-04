02175622bf27e8b0020f5c07d8884c12c07e089e
ahujask <allen@missionvi.com>
2018-12-04 14:26:29 -0500

    fix error from crypto exchange scene when signing out

--- src/components/scenes/ExchangeSettingsScene.js
+++ src/components/scenes/ExchangeSettingsScene.js
@@ -122,7 +122,7 @@ export class ExchangeSettingsComponent extends Component<ExchangeSettingsProps, 
                     />
                     <RowWithButton
                       logo={logoSource}
-                      key={exchange.name}
+                      key={exchange.name + '1'}
                       leftText={exchangeName + ' ' + s.strings.account}
                       rightText={ssLoginText}
                       onToggle={this.shapeShiftSignInToggle}
